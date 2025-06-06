556fb2591b3614ac38b71364626e7f42b04889df
SamuelSudeepAyyala <ayyalasamuelsudeep@gmail.com>
2025-06-05 21:08:30 -0400

    Added CI/CD pipeline for alert config and added environment secrets

---
+++ Week2 - Alerting and Incident Detection/generate_config.py
@@ -0,0 +1,16 @@
+import os
+
+template_path = "./alertmanager/config.yml"
+output_path = "./alertmanager/config.yml"
+
+with open(template_path, "r") as f:
+    config = f.read()
+
+for key in ["EMAIL_USERNAME", "EMAIL_PASSWORD", "EMAIL_RECEIVER"]:
+    value = os.environ.get(key, "")
+    print(f"Replacing ${key} with {value}")
+    config = config.replace(f"${{{key}}}", value)
+
+with open(output_path, "w") as f:
+    print("Writing into the output file")
+    f.write(config)
