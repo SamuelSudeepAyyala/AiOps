69701224f861cc223f005db53d52ce80cd4fe1d9
SamuelSudeepAyyala <ayyalasamuelsudeep@gmail.com>
2025-06-10 20:30:01 -0400

    Enriched the webhook log

--- Week2 - Alerting and Incident Detection/webhook/webhook_receiver.py
+++ Week2 - Alerting and Incident Detection/webhook/webhook_receiver.py
@@ -7,8 +7,16 @@ logging.basicConfig(level=logging.INFO)
 @app.route('/', methods=['POST'])
 def alert():
     alert_data = request.json
-    app.logger.info("🚨 Webhook Alert Received:")
-    app.logger.info(alert_data)
+    for alert in alert_data.get('alerts', []):
+        enriched_log = {
+            "Alert": alert.get("labels", {}).get("alertname"),
+            "Instance": alert.get("labels", {}).get("instance"),
+            "Severity": alert.get("labels", {}).get("severity"),
+            "Summary": alert.get("annotations", {}).get("summary"),
+            "Description": alert.get("annotations", {}).get("description"),
+            "Time Fired": alert.get("startsAt")
+        }
+        app.logger.info(f"🚨 Webhook Alert Received:\n{enriched_log}")
     return jsonify({"status": "received"}), 200
 
 if __name__ == '__main__':
